ed31834b779dd904fab90e515d565b13326c2e49
Clarice Costello <claricostello@outlook.com>
2019-02-13 10:12:28 -0800

    configured startup and dbcontext

---
+++ DotNetGram/DotNetGram/Data/PostDbContext.cs
@@ -0,0 +1,23 @@
+﻿using DotNetGram.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetGram.Data
+{
+    public class PostDbContext : DbContext
+    {
+        public PostDbContext(DbContextOptions<PostDbContext> options) : base(options)
+        {
+
+        }
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+        public DbSet<Post> Posts { get; set; }
+    }
+}
---
+++ DotNetGram/DotNetGram/Models/Services/PostService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using
+using DotNetGram.Models.Interfaces;
+
+namespace DotNetGram.Models.Services
+{
+    public class PostService : IPost
+    {
+        public Task Delete(int id)
+        {
+            
+        }
+
+        public Task<Post> FindPost(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Post>> GetPosts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveAsync(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

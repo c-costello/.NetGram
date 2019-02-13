727972bb347a63d657e79105b19eb084b80f4963
Clarice Costello <claricostello@outlook.com>
2019-02-13 09:57:03 -0800

    added post model

---
+++ DotNetGram/DotNetGram/Models/Post.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetGram.Models
+{
+    public class Post
+    {
+        public int ID { get; set; }
+        public string Image { get; set; }
+        public string Poster { get; set; }
+        public string Description { get; set; }
+
+    }
+}

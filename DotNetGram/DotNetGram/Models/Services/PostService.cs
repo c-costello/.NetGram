c89f47ce3b7384488743bf9b3bffe8309b3dac65
Clarice Costello <claricostello@outlook.com>
2019-02-13 10:37:35 -0800

    built out posts service

--- DotNetGram/DotNetGram/Models/Services/PostService.cs
+++ DotNetGram/DotNetGram/Models/Services/PostService.cs
@@ -2,30 +2,50 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DotNetGram.Data;
 using DotNetGram.Models.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace DotNetGram.Models.Services
 {
     public class PostService : IPost
     {
-        public Task Delete(int id)
+        private readonly PostDbContext _context;
+
+        public PostService(PostDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        public async Task Delete(int id)
+        {
+            Post post = await _context.Posts.FindAsync(id);
+            _context.Posts.Remove(post);
+
         }
 
-        public Task<Post> FindPost(int id)
+        public async Task<Post> FindPost(int id)
         {
-            throw new NotImplementedException();
+            Post post = await _context.Posts.FindAsync(id);
+            return post;
         }
 
-        public Task<List<Post>> GetPosts()
+        public async Task<List<Post>> GetPosts()
         {
-            throw new NotImplementedException();
+            var posts = await _context.Posts.ToListAsync();
+            return posts;
         }
 
-        public Task SaveAsync(Post post)
+        public async Task SaveAsync(Post post)
         {
-            throw new NotImplementedException();
+            if (await _context.Posts.FirstOrDefaultAsync(p => p.ID == post.ID) == null)
+            {
+                _context.Posts.Add(post);
+            }
+            else
+            {
+                _context.Update(post);
+            }
+            await _context.SaveChangesAsync();
         }
     }
 }
